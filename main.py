81d3514f91521fecb61a472f8ff64abe35cec496
Vinicius-klaus <vg222gy@student.lnu.se>
2022-12-20 15:10:43 -0300

    Change end point and delete not used funciotons

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Union
+
 import time
 
 from fastapi import FastAPI
@@ -6,12 +6,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
+@app.get("/time")
+def read_local_time():
     current_time = time.ctime()
     return current_time
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
